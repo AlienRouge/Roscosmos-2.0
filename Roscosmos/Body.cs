29e6610304be08bbb17bee2635f9b74d276604cd
AlienRouge <rudov.vlad2013@gmail.com>
2021-02-19 12:11:22 +0700

    Add project files.

---
+++ Roscosmos/Body.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roscosmos
+{
+    public static class Body
+    {
+        private const double g = 9.81;
+        private const double C = 0.15;
+        private const double rho = 1.29;
+
+        private static double Vx;
+        private static double Vy;
+        private static double K;
+
+        public static double Y0;
+        public static double V0;
+        public static double Angle;
+        public static double Area;
+        public static double Mass;
+
+
+        public static void InitFlightParams()
+        {
+            Vx = V0 * Math.Cos(Angle * Math.PI / 180);
+            Vy = V0 * Math.Sin(Angle * Math.PI / 180);
+            K = 0.5 * C * Area * rho / Mass;
+        }
+
+        public static void CalculatePosition(ref (double, double) pos, double dt)
+        {
+            Vx -= K * Vx * Math.Sqrt(Vx * Vx + Vy * Vy) * dt;
+            Vy -= (g + K * Vy * Math.Sqrt(Vx * Vx + Vy * Vy)) * dt;
+
+            pos.Item1 += Vx * dt;
+            pos.Item2 += Vy * dt;
+        }
+    }
+}
---
+++ Roscosmos/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows.Forms;
+using Roscosmos.Properties;
+
+namespace Roscosmos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private (double, double) pos;
+
+        private double t;
+        private const double dt = 0.01;
+
+        private void launchButton_Click(object sender, EventArgs e)
+        {
+            pauseButton.BackgroundImage = Resources.circled_pause;
+            pauseButton.Visible = true;
+
+            Body.Y0 = (double) edHeight.Value;
+            Body.V0 = (double) edSpeed.Value;
+            Body.Angle = (double) edAngle.Value;
+            Body.Area = (double) edArea.Value;
+            Body.Mass = (double) edMass.Value;
+
+            pos.Item1 = 0;
+            pos.Item2 = Body.Y0;
+            t = 0;
+
+            Body.InitFlightParams();
+
+            chart1.Series[0].Points.Clear();
+            chart1.Series[0].Points.AddXY(pos.Item1, pos.Item2);
+
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label4.Text = Math.Round(t, 2).ToString();
+
+            t += dt;
+            Body.CalculatePosition(ref pos, dt);
+            chart1.Series[0].Points.AddXY(pos.Item1, pos.Item2);
+            if (!(pos.Item2 <= 0)) return;
+            timer1.Stop();
+            pauseButton.Visible = false;
+        }
+
+        private void pauseButton_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;
+            pauseButton.BackgroundImage =
+                timer1.Enabled ? Resources.circled_pause : Resources.circled_play;
+        }
+    }
+}
